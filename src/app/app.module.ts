6bab339197b16c973f0844f42e59b4af2ac15d2c
Begolas <begolas@gmx.de>
2023-05-28 11:14:07 +0200

    add: Dashboard Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,47 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-}), BrowserAnimationsModule],
-  providers: [],
+  declarations: [AppComponent, NavigationComponent, DashboardComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+  ],
+  providers: [
+    { provide: FirebaseOptionsToken, useValue: environment.firebaseConfig },
+    { provide: FirebaseAppNameToken, useValue: 'stalldata' },
+    { provide: FirebaseAppConfigToken, useValue: undefined },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCfjOayGJqPUR21aDUpKlO2zLz44kBVTRc',
+
+    authDomain: 'stallverwaltung-536b5.firebaseapp.com',
+
+    projectId: 'stallverwaltung-536b5',
+
+    storageBucket: 'stallverwaltung-536b5.appspot.com',
+
+    messagingSenderId: '686802941321',
+
+    appId: '1:686802941321:web:b8c4ebd79c48aabbb85d58',
+  },
+};
